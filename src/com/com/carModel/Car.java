273cf8fff25276b3803639a590fcf34f44a83934
berkay <berkayselvi@gmail.com>
2019-03-28 15:44:10 +0300

    commit

---
+++ src/com/com/carModel/Car.java
@@ -0,0 +1,20 @@
+package com.com.carModel;
+
+public class Car {
+    private int doors;
+    private int wheels;
+    private String model;
+
+    public void setModel(String model){
+        String validModel = model.toLowerCase();
+        if(validModel.equals("carrera") || validModel.equals("commodore")){
+            this.model = model;
+        }
+        else {
+            this.model = "Unknown";
+        }
+    }
+    public String getModel(){
+        return this.model;
+    }
+}
