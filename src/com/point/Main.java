341c120fd8577576f97a0a2c1b5677c1316b0a3f
berkay <berkayselvi@gmail.com>
2019-03-28 15:18:15 +0300

    commit

--- src/com/point/Main.java
+++ src/com/point/Main.java
@@ -1,4 +1,4 @@
-package com.berkayselvi;
+package com.point;
 
 public class Main {
     public static void main(String[] args) {
--- src/com/point/Point.java
+++ src/com/point/Point.java
@@ -1,4 +1,4 @@
-package com.berkayselvi;
+package com.point;
 
 public class Point {
     private int x;
@@ -37,8 +37,7 @@ public class Point {
         return (Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y)));
     }
 
-    public double distance(Point){
-
+    public double distance(Point point){
+        return (Math.sqrt((point.x - this.x) * (point.x - this.x) + (point.y - this.y) * (point.y - this.y)));
     }
-
 }
